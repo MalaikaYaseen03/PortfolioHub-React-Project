edb8da53e9987bc4c6d75ddaef3456ee3da66122
MalaikaYaseen03 <malaikayaseen03@gmail.com>
2025-01-18 17:51:07 +0500

    Created about section and imported it in Home component

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import PersonalSkills from "../PersonalSkills/PersonalSkills";
 import useFetch from "../CustomHook/useFetch";
+import About from "../About/About";
 const Home = () => {
   const API_URL = useMemo(
     () => process.env.REACT_APP_BACKEND_URL || "http://localhost:8080",
@@ -8,12 +9,14 @@ const Home = () => {
   );
 
   const { data: personalSkills } = useFetch(`${API_URL}/api/v1/personalSkills`);
+  const { data: about } = useFetch(`${API_URL}/api/v1/about`);
   return (
     <>
       <main id="main">
         {personalSkills && (
           <PersonalSkills personalSkills={personalSkills || []} />
         )}
+        {about && <About about={about || []} />}
       </main>
     </>
   );
