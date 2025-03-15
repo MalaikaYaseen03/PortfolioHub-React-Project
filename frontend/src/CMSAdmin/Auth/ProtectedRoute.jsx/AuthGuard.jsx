52de944d460e9bdb52fce485fa62f8e308f379a4
MalaikaYaseen03 <malaikayaseen03@gmail.com>
2025-03-15 23:41:57 +0500

    Added component for protected routes

---
+++ frontend/src/CMSAdmin/Auth/ProtectedRoute.jsx/AuthGuard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../AuthContext/AuthContext";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isAuthenticated, isLoading } = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (isLoading) {
+          return <div>Loading...</div>; // Show loader while loading
+        }
+        return isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/form/login-form" />
+        );
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
