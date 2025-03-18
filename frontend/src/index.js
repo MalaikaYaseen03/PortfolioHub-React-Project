9429a9309b94407a200deab0bca7b22e58602eea
MalaikaYaseen03 <malaikayaseen03@gmail.com>
2025-03-18 23:27:03 +0500

    Wrapped the app with auth provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { FetchCacheProvider } from "./Components/Contexts/FetchCacheContext";
+import { AuthProvider } from "./CMSAdmin/Auth/AuthContext/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/*Context for caching data*/}
     <FetchCacheProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      {/* Authentication Context for managing user authentication */}
+      <AuthProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </AuthProvider>
     </FetchCacheProvider>
   </React.StrictMode>
 );
